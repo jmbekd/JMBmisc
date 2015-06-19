01b4e757dd4f272cd27f684933265c142eed2564
jmbekd <jmbekd@gmail.com>
2015-06-18 21:58:05 -0700

    updated plot_xs.R, plot_xs_locs.R and documentation.

--- R/plot_xs.R
+++ R/plot_xs.R
@@ -7,10 +7,10 @@
 #' information, and the cross section parameters set up using the xs_params function, an empty
 #' gridded cross section will be plotted.
 #'
-#' @param data A data.frame or data.table with columns of "distance" (the maximum desired
-#'   distance of the cross section), "depth" (the maximum depth below ground surface), and
-#'   "gs_elev" (ground surface elevation). This parameter is not necessary if the distance,
-#'   depth, and gs_elev parameters are individually specified (see below). Defaults to NULL.
+#' @param data A data.frame or data.table with columns of "distance", "depth", and
+#'   "gs_elev". For explanations of these parameters and their valid values, see below. The data
+#'   parameter is not necessary if the distance, depth, and gs_elev parameters are individually
+#'   specified. Defaults to NULL.
 #' @param distance Horizontal distance of the boreholes and/or wells along the cross section or
 #'   the maximum distance that the cross section should extend. Can be a single value or a
 #'   vector of distances. If a vector of values is supplied, the maximum value is selected.
@@ -34,22 +34,22 @@
 #'   the upper limit being the rounded gs_elev value and the lower limit
 #'   being the rounded gs_elev - and the depth (see depth).
 #' @param name The cross section ID (e.g., A, B, etc.). Defaults to NULL.
-#' @param xs_params A list of cross section parameters from the xs_params function (see
+#' @param xs_par A list of cross section parameters from the xs_params function (see
 #'   ?xs_params). Defaults to xs_params().
 #'
 #' @return A plot of an empty gridded cross section.
 #'
-#' @keywords xs, cross section, plot, gs_elev, depth, distance, xs_params
+#' @keywords xs, cross section, plot, gs_elev, depth, distance, xs_params, grid
 #'
 #' @export
 #'
 #' @examples
-#' plot_xs(distance = c(100, 200, 600), depth = c(30, 15, 55), gs_elev = c(10, 12, 11),
+#' grid_xs(distance = c(100, 200, 600), depth = c(30, 15, 55), gs_elev = c(10, 12, 11),
 #' name = "A", xs_params = xs_params())
 
-plot_xs <- function(data = NULL,
+grid_xs <- function(data = NULL,
                     distance = NULL, depth = NULL, gs_elev = NULL,
-                    name = NULL, xs_params = xs_params()) {
+                    name = NULL, xs_par = xs_params()) {
 
   if (!is.null(data)) {
     names <- names(data)
@@ -65,15 +65,12 @@ plot_xs <- function(data = NULL,
   }
 
   if (length(distance) > 1) {
-    warning("more than one distance specified. only the maximum value will be used.")
     distance <- max(distance)
   }
   if (length(depth) > 1) {
-    warning("more than one depth specified. only the maximum value will be used.")
     depth <- max(depth)
   }
   if (length(gs_elev) > 1) {
-    warning("more than one gs_elev specified. only the maximum value will be used.")
     gs_elev <- max(gs_elev)
   }
 
@@ -83,15 +80,15 @@ plot_xs <- function(data = NULL,
   if (is.null(gs_elev)) gs_elev <- max(data[["gs_elev"]])
 
   ## round the distance, depth, and gs_elev parameters
-  distance <- plyr::round_any(distance, xs_params[["h_major"]], f = ceiling)
-  depth <- plyr::round_any(depth, xs_params[["v_major"]], f = ceiling)
-  gs_elev <- plyr::round_any(gs_elev, xs_params[["v_minor"]], f = ceiling)
+  distance <- plyr::round_any(distance, xs_par[["h_major"]], f = ceiling)
+  depth <- plyr::round_any(depth, xs_par[["v_major"]], f = ceiling)
+  gs_elev <- plyr::round_any(gs_elev, xs_par[["v_minor"]], f = ceiling)
 
   ## determine the figure height and width and x and y axes limits
   y_lim <- range(pretty(c(gs_elev, gs_elev - depth)))
   x_lim <- range(pretty(c(0, distance)))
-  fig_height <- diff(y_lim) / xs_params[["v_scale"]]
-  fig_width <- diff(x_lim) / xs_params[["h_scale"]]
+  fig_height <- diff(y_lim) / xs_par[["v_scale"]]
+  fig_width <- diff(x_lim) / xs_par[["h_scale"]]
 
   par(pin = c(fig_width, fig_height), yaxs = "i", xaxs = "i",
       mar = c(5, 5, 5, 5)) # a value of 5 lines was chosen as 5 lines = 1 inch
@@ -99,41 +96,41 @@ plot_xs <- function(data = NULL,
   ## set up plotting region
   plot(0, 0, type = "n",
        xlim = x_lim, ylim = y_lim, axes = FALSE,
-       xlab = paste0("Distance (" , xs_params[["units"]], ")"),
-       ylab = paste0("Elevation (" , xs_params[["units"]], ")"),
+       xlab = paste0("Distance (" , xs_par[["units"]], ")"),
+       ylab = paste0("Elevation (" , xs_par[["units"]], ")"),
        main = paste0("Cross Section ", name, "-", name, "'"))
 
   ## add major and minor tick marks to x and y axes
   axis(1, at = seq(from = x_lim[1], to = x_lim[2],
-                   by = xs_params[["h_minor"]]), label = FALSE, tck = -0.01)
+                   by = xs_par[["h_minor"]]), label = FALSE, tck = -0.01)
   axis(1, at = seq(from = x_lim[1], to = x_lim[2],
-                   by = xs_params[["h_major"]]))
+                   by = xs_par[["h_major"]]))
   axis(2, at = seq(from = y_lim[1], to = y_lim[2],
-                   by = xs_params[["v_minor"]]), label = FALSE, tck = -0.01)
+                   by = xs_par[["v_minor"]]), label = FALSE, tck = -0.01)
   axis(2, at = seq(from = y_lim[1], to = y_lim[2],
-                   by = xs_params[["v_major"]]), las = 2)
+                   by = xs_par[["v_major"]]), las = 2)
   axis(4, at = seq(from = y_lim[1], to = y_lim[2],
-                   by = xs_params[["v_minor"]]), label = FALSE, tck = -0.01)
+                   by = xs_par[["v_minor"]]), label = FALSE, tck = -0.01)
   axis(4, at = seq(from = y_lim[1], to = y_lim[2],
-                   by = xs_params[["v_major"]]), las = 2)
-  mtext(text = paste0("Elevation (" , xs_params[["units"]], ")"), side = 4,
+                   by = xs_par[["v_major"]]), las = 2)
+  mtext(text = paste0("Elevation (" , xs_par[["units"]], ")"), side = 4,
         line = 3)
 
   ## add major and minor gridlines to x and y axes
-  if (xs_params[["plot_h_minor"]]) {
-    abline(v = seq(from = x_lim[1], to = x_lim[2], by = xs_params[["h_minor"]]),
+  if (xs_par[["plot_h_minor"]]) {
+    abline(v = seq(from = x_lim[1], to = x_lim[2], by = xs_par[["h_minor"]]),
            lty = 3, lwd = 0.3)
   }
-  if (xs_params[["plot_h_major"]]) {
-    abline(v = seq(from = x_lim[1], to = x_lim[2], by = xs_params[["h_major"]]),
+  if (xs_par[["plot_h_major"]]) {
+    abline(v = seq(from = x_lim[1], to = x_lim[2], by = xs_par[["h_major"]]),
            lty = 3, lwd = 0.5)
   }
-  if (xs_params[["plot_v_minor"]]) {
-    abline(h = seq(from = y_lim[1], to = y_lim[2], by = xs_params[["v_minor"]]),
+  if (xs_par[["plot_v_minor"]]) {
+    abline(h = seq(from = y_lim[1], to = y_lim[2], by = xs_par[["v_minor"]]),
            lty = 3, lwd = 0.3)
   }
-  if (xs_params[["plot_v_major"]]) {
-    abline(h = seq(from = y_lim[1], to = y_lim[2], by = xs_params[["v_major"]]),
+  if (xs_par[["plot_v_major"]]) {
+    abline(h = seq(from = y_lim[1], to = y_lim[2], by = xs_par[["v_major"]]),
            lty = 2, lwd = 0.5)
   }
 }
--- R/plot_xs_locs.R
+++ R/plot_xs_locs.R
@@ -1,41 +1,60 @@
 #' Plot wells/boreholes on the cross section
 #'
-#' This function plots wells and/or boreholes on the cross section
+#' This function plots wells and/or boreholes on the cross section. The function will plot a
+#' rectangular screened interval for wells. The bottom of a well is denoted with a dash and
+#' the bottom of a borehole is denoted with a point.
 #'
-#' @param v_scale Vertical scale of the y-axis. Defaults to 10 (i.e.,
-#'   in the resulting plot, 10 feet on the y-axis will correspond to 1 inch printed).
-#' @param h_scale Horizontal scale of the x-axis. Defaults to 100 (i.e.,
-#'   in the resulting plot, 100 feet on the x-axis will correspond to 1 inch printed).
-#' @param v_minor Interval for plotting minor horizontal grid lines. Defaults to 1 (i.e.,
-#'   a minor gridline every foot).
-#' @param v_major Interval for plotting major horizontal grid lines. Defaults to 10 (i.e.,
-#'   a major gridline every 10 feet).
-#' @param h_minor Interval for plotting minor vertical grid lines. Defaults to 20 (i.e.,
-#'   a minor gridline every 20 feet).
-#' @param h_major Interval for plotting major vertical grid lines. Defaults to 100 (i.e.,
-#'   a major gridline every 100 feet).
-#' @param h_major Interval for plotting major vertical grid lines. Defaults to 100 (i.e.,
-#'   a major gridline every 100 feet).
-#' @param units Unit of measurement for the x and y axes. Defaults to feet ("ft").
-#' @param plot_v_minor Plot minor horizontal grid lines? Defaults to TRUE.
-#' @param plot_v_major Plot major horizontal grid lines? Defaults to TRUE.
-#' @param plot_h_minor Plot minor vertical grid lines? Defaults to FALSE.
-#' @param plot_h_major Plot major vertical grid lines? Defaults to TRUE.
+#' @param dt A data.frame or data.table with columns of "location", "type", "distance",
+#'   "gs_elev", "depth", "tos", and "bos". For explanations of these parameters and their
+#'   valid values, see the definitions below. The data parameter is not necessary if the
+#'   location, type, distance, gs_elev, depth, tos, and bos parameters are individually
+#'   specified. Defaults to NULL.
+#' @param location A unique identifier for each borehole and/or well (e.g., "MW-3", "BH-45",
+#'   etc.). Should be a vector of character strings. Defaults to NULL.
+#' @param type Is the specified location a well, borehole, etc.? Should be a vector of character
+#'   strings such as "well", "borehole", "ssp", "svp", etc. Defaults to NULL.
+#' @param distance Horizontal distance of each of the boreholes and/or wells along the cross
+#'   section. Should be a vector of depths. Defaults to NULL.
+#' @param gs_elev The ground surface elevation at each of the boreholes and/or wells along the
+#'   cross section. Should be a vector of depths. Defaults to NULL.
+#' @param depth The total depth (below ground surface) at each of the boreholes and/or wells
+#'   along the cross section. Should be a vector of depths. Defaults to NULL.
+#' @param tos The distance from the ground surface to the top of the screened interval for
+#'   each well along the cross section. Should be a vector of depths. Defaults to NULL.
+#' @param bos The distance from the ground surface to the tbottom of the screened interval for
+#'   each well along the cross section. Should be a vector of depths. Defaults to NULL.
+#' @param plot_gs. Should a line be plotted between the ground surface elevations (gs_elev)
+#'   of the boreholes and wells? Defaults to TRUE.
+#' @param w_width Width of the well screen in inches. Defaults to 0.1.
+#' @param label_shift The vertical distance by which to shift the borehole and/or well
+#'   labels. Defaults to 2.
+#' @param xs_params A list of cross section parameters from the xs_params function (see
+#'   ?xs_params). Defaults to xs_params().
 #'
-#' @return List of parameters that will be used to set desired cross section parameters.
+#' @return Plots the wells and boreholes on the cross section set up using grid_xs().
 #'
-#' @keywords xs, params, cross section
+#' @keywords xs, plot, cross section
 #'
 #' @export
 #'
 #' @examples
-#' xs_params()
+#' grid_xs(distance = c(100, 200, 550),
+#'         depth = c(30, 15, 55),
+#'         gs_elev = c(10, 12, 11),
+#'         name = "A", xs_par = xs_params())
+#' plot_xs_locs(location = c("MW-1", "BH-5", "CPT-3"),
+#'              type = c("well", "borehole", "cpt"),
+#'              distance = c(100, 200, 550),
+#'              gs_elev = c(11, 8, 12),
+#'              depth = c(60, 42, 90),
+#'              tos = c(45, NA, NA),
+#'              bos = c(55, NA, NA))
 
-plot_xs_locs <- function(data = null,
-                         location = null, type = null, distance = null, gs_elev = null,
-                         depth = null, tos = null, bos = null,
-                         plot_gs = TRUE, w_width = 0.1, label_shift = 10,
-                         cross_section_params = cross_section_params()) {
+plot_xs_locs <- function(dt = NULL,
+                         location = NULL, type = NULL, distance = NULL, gs_elev = NULL,
+                         depth = NULL, tos = NULL, bos = NULL,
+                         plot_gs = TRUE, w_width = 0.1, label_shift = 2,
+                         xs_par = xs_params()) {
 
   ## data should be a data.frame or data.table with columns of "location", "type" (e.g., well,),
   ## borehole, etc.), "distance", "gs_elev" (ground surface elevation), "depth", "tos" (top of screen),
@@ -45,55 +64,67 @@ plot_xs_locs <- function(data = null,
   ## label_shift is the vertical distance to shift the well/borehole label above the top of the
   ## ground surface.
 
-  if (!is.null(data)) {
-    names <- names(data)
+  if (!is.null(dt)) {
+    names <- names(dt)
     if (any(!(c("location", "type", "distance", "gs_elev",
                 "depth", "tos", "bos") %in% names))) {
       stop ("please provide a data.frame with columns identified as 'location', 'type',
             'distance', 'gs_elev', 'depth', 'tos', and 'bos' or specify the vectors of 'location',
             'type', 'distance', 'gs_elev', 'depth', 'tos', and 'bos' directly.")
     }
-    }
+    location <- dt[["location"]]
+    type <- dt[["type"]]
+    distance <- dt[["distance"]]
+    gs_elev <- dt[["gs_elev"]]
+    depth <- dt[["depth"]]
+    tos <- dt[["tos"]]
+    bos <- dt[["bos"]]
+  }
 
-  if (is.null(data) & any(is.null(location) & is.null(distance) & is.null(gs_elev) &
+  if (is.null(dt) & any(is.null(location) & is.null(distance) & is.null(gs_elev) &
                           is.null(depth) & is.null(tos) & is.null(bos))) {
     stop ("please provide a data.frame or data.table with columns identified as 'location', 'type',
           'distance', 'gs_elev', 'depth', 'tos', and 'bos' or specify the vectors of 'location',
           'type', 'distance', 'gs_elev', 'depth', 'tos', and 'bos' directly.")
   }
 
-  ## if the location, type, distance, gs_elev, depth, tos, and bos parameters not specified,
-  ## determine their values from data
-  if (is.null(location)) location <- data[["location"]]
-  if (is.null(type)) location <- data[["type"]]
-  if (is.null(distance)) distance <- data[["distance"]]
-  if (is.null(gs_elev)) gs_elev <- data[["gs_elev"]]
-  if (is.null(depth)) depth <- data[["depth"]]
-  if (is.null(tos)) tos <- data[["tos"]]
-  if (is.null(tos)) bos <- data[["bos"]]
-
   if (plot_gs) {
-    lines(x = distance, y = gs_elev, lty = 1, lwd = 1.5)
+    lines(x = distance,
+          y = gs_elev,
+          lty = 1, lwd = 1.5)
   }
 
   ## plot a line from the ground surface to the bottom of the well/borehole
-  segments(x0 = distance, y0 = gs_elev,
-           x1 = distance, y1 = gs_elev - depth,
+  segments(x0 = distance,
+           y0 = gs_elev,
+           x1 = distance,
+           y1 = gs_elev - depth,
            lty = 1, lwd = 1, lend = "butt")
 
+  wells <- tolower(type) == "well"
+
   ## plot a tick mark at the bottom of the well
-  if (type == "well") {
-    segments(x0 = distance - cross_section_params[["h_scale"]] * w_width / 2, y0 = gs_elev - depth,
-             x1 = distance + cross_section_params[["h_scale"]] * w_width / 2, y1 = gs_elev - depth,
-             lty = 1, lwd = 2, lend = "butt")
-    ## plot a rectangle to the left of the well showing the screened interval
-    rect(xleft = distance - cross_section_params[["h_scale"]] * w_width, ybottom = gs_elev - bos,
-         xright = distance, ytop = gs_elev - tos, density = 24, angle = 0,
-         col = "lightgrey")
-  } else {
-    points(distance, gs_elev - depth, pch = 19)
-  }
+  segments(x0 = distance[wells] - xs_par[["h_scale"]] * w_width / 2,
+           y0 = gs_elev[wells] - depth[wells],
+           x1 = distance[wells] + xs_par[["h_scale"]] * w_width / 2,
+           y1 = gs_elev[wells] - depth[wells],
+           lty = 1, lwd = 2, lend = "butt")
+
+  ## plot a rectangle to the left of the well showing the screened interval
+  rect(xleft = distance[wells] - xs_par[["h_scale"]] * w_width,
+       ybottom = gs_elev[wells] - bos[wells],
+       xright = distance[wells],
+       ytop = gs_elev[wells] - tos[wells],
+       density = 24, angle = 0)
+
+  ## plot a point at the total depth of each non-well borehole
+  points(x = distance[!wells],
+         y = gs_elev[!wells] - depth[!wells],
+         pch = 19)
 
   ## add location labels
-  text(x = distance, y = elev + label_shift, labels = location, cex = 0.5, srt = 90, adj = c(0, 0.25))
-  }
+  text(x = distance,
+       y = gs_elev + label_shift,
+       labels = location,
+       cex = 0.5, srt = 90, adj = c(0, 0.25), xpd = TRUE)
+}
